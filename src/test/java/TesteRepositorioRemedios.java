54e182f0070e3a472528738609101319ab7e9e58
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-05-13 19:12:32 -0300

    fix: TesteRepositorioRemedios

--- src/test/java/TesteRepositorioRemedios.java
+++ src/test/java/TesteRepositorioRemedios.java
@@ -10,13 +10,16 @@ import java.util.Date;
 public class TesteRepositorioRemedios {
     static boolean testeRepositorioRemedios() {
         ArrayList<Remedio> resposta = new ArrayList<>();
+        RemedioRepository repository = new RemedioRepository();
         Substancia acido = new Substancia(Substancia.Tipo.Outros, "Acido");
         Farmaceutica drogasil = new Farmaceutica("Zé", 999, "Fulano");
-        Remedio acidoSufurico = new Remedio(acido.meuTipo, acido.nome_susbstancia, "Acido Sufurico", new Date(2024, Calendar.DECEMBER, 2), 10, 05F, drogasil);
+        Remedio acidoSufurico = new Remedio(acido.meuTipo, acido.nome_susbstancia, "Acido Sufurico",
+                                new Date(2024, Calendar.DECEMBER, 2), 10, 05F, drogasil);
         resposta.add(acidoSufurico);
-
-        RemedioRepository repository = new RemedioRepository();
         repository.save(acidoSufurico);
-        return resposta.equals(repository.encontrarTodosRemedios());
+
+        boolean teste = resposta.equals(repository.encontrarTodosRemedios());
+        System.out.println(teste ? "O repositório está funcionando." : "O repositório não está funcionando.");
+        return teste;
     }
 }
