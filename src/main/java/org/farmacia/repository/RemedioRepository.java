57b140cd3e7cc73dfb90a41ebdd801c728b86cc5
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-05-13 18:20:45 -0300

    fix: Repositório de remédios corrigido.

--- src/main/java/org/farmacia/repository/RemedioRepository.java
+++ src/main/java/org/farmacia/repository/RemedioRepository.java
@@ -2,7 +2,6 @@ package org.farmacia.repository;
 
 import org.farmacia.entities.Farmaceutica;
 import org.farmacia.entities.Remedio;
-import org.farmacia.entities.Substancia;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,56 +9,83 @@ import java.util.Objects;
 
 public class RemedioRepository {
 
+    /*
+    Repositório de Remédios: Armazena remédios em memória em uma ArrayList
+    save(Remedio):void -> Adiciona remédio ao banco de dados,
+    encontrarPorNome(String nome): ArrayList<Remedio> -> Pesquisa por um nome e retorna remédios com este nome,
+    encontrarPorValidade(Date validade): ArrayList<Remedio> -> Pesquisa por um nome e retorna remédios com esta validade,
+    encontrarPorFarmaceutica(String farmaceutica): ArrayList<Remedio> -> Pesquisa por um nome e retorna remédios desta farmaceutica,
+    encontrarTodosRemedios(): ArrayList<Remedio> -> Retorna todos remédios cadastrados,
+    atualizarRemedio(String nome, String farmaceutica, String novoNome, Date dataVencimento, int quantidade,
+                     float concentracao, Farmaceutica novaFarmaceutica): void -> Atualiza remédio x de farmaceutica y
+                     com os dados passados.
+    removerRemedio(String nome, String farmaceutica): void -> Remove remédio x de farmaceutica y do sistema.
+    */
+
     ArrayList<Remedio> remedios;
 
     public void save(Remedio remedio) {
         remedios.add(remedio);
     }
 
-    public Remedio encontrarPorNome(String nome) {
+    public ArrayList<Remedio> encontrarPorNome(String nome) {
+        ArrayList<Remedio> remediosPesquisados = new ArrayList<>();
         for (Remedio remedio : remedios) {
             if (Objects.equals(remedio.nome_remedio, nome)) {
-                return remedio;
+                remediosPesquisados.add(remedio);
             }
         }
-        return null;
+        return remediosPesquisados;
     }
 
-    public Remedio encontrarPorValidade(Date validade) {
+    public ArrayList<Remedio> encontrarPorValidade(Date validade) {
+        ArrayList<Remedio> remediosPesquisados = new ArrayList<>();
         for (Remedio remedio : remedios) {
             if (Objects.equals(remedio.data_vencimento, validade)) {
-                return remedio;
+                remediosPesquisados.add(remedio);
             }
         }
-        return null;
+        return remediosPesquisados;
     }
 
-    public Remedio encontrarPorFarmaceutica(String nome_farmaceutica) {
+    public ArrayList<Remedio> encontrarPorFarmaceutica(String farmaceutica) {
+        ArrayList<Remedio> remediosPesquisados = new ArrayList<>();
         for (Remedio remedio : remedios) {
-            if (Objects.equals(remedio.farmaceutica.nome_farmaceutica, nome_farmaceutica)) {
-                return remedio;
+            if (Objects.equals(remedio.farmaceutica.nome_farmaceutica, farmaceutica)) {
+                remediosPesquisados.add(remedio);
             }
         }
-        return null;
+        return remediosPesquisados;
     }
 
     public ArrayList<Remedio> encontrarTodosRemedios() {
         return remedios;
     }
 
-    public void atualizarRemedio(String antigo_nome, Substancia.Tipo tipo, String nome, String nome_remedio, Date data_vencimento, int quantidade, float concentracao, Farmaceutica farmaceutica){
-        Remedio remedio = encontrarPorNome(antigo_nome);
-        remedios.remove(remedio);
-        remedio.setNome_remedio(nome_remedio);
-        remedio.setData_vencimento(data_vencimento);
-        remedio.setQuantidade(quantidade);
-        remedio.setConcentracao(concentracao);
-        remedio.setFarmaceutica(farmaceutica);
-        remedios.add(remedio);
+    public void atualizarRemedio(String nome, String farmaceutica, String novoNome, Date dataVencimento, int quantidade,
+                                 float concentracao, Farmaceutica novaFarmaceutica) {
+        ArrayList<Remedio> remediosPesquisados = encontrarPorNome(nome);
+        for (Remedio remedio : remediosPesquisados) {
+            if (Objects.equals(remedio.farmaceutica.nome_farmaceutica, farmaceutica)) {
+                remedio.setNome_remedio(novoNome);
+                remedio.setData_vencimento(dataVencimento);
+                remedio.setQuantidade(quantidade);
+                remedio.setConcentracao(concentracao);
+                remedio.setFarmaceutica(novaFarmaceutica);
+            } else {
+                System.out.println("Remédio não encontrado.");
+            }
+        }
     }
 
-    public void removerRemedio(String nome) {
-        Remedio remedio = encontrarPorNome(nome);
-        remedios.remove(remedio);
+    public void removerRemedio(String nome, String farmaceutica) {
+        ArrayList<Remedio> remediosPesquisados = encontrarPorNome(nome);
+        for (Remedio remedio : remediosPesquisados) {
+            if (Objects.equals(remedio.farmaceutica.nome_farmaceutica, farmaceutica)) {
+                remedios.remove(remedio);
+            } else {
+                System.out.println("Remédio não encontrado.");
+            }
+        }
     }
 }
