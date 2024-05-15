469f06a0570085f034adcb27490bfbbe408122d8
Tizuey <89665231+Tizuey@users.noreply.github.com>
2024-05-15 17:21:42 -0300

    Update Substancia.java
    Tizuey -> comentado

--- src/main/java/org/farmacia/entities/Substancia.java
+++ src/main/java/org/farmacia/entities/Substancia.java
@@ -1,20 +1,20 @@
 package org.farmacia.entities;
-
+// aqui é a classe de Substancia no qual se criara o objeto
 public class Substancia {
-
+// aqui para enumerar o tipos de substancias
     public enum Tipo {
         Alzheimer_Demencia, Antidrepressivo, Antipsicotico, Parkinson, Antinflamatorio_DorDeCabeca, Outros, Vitamina
     }
 
     public Tipo meuTipo;
     public String nome_susbstancia;
-
+    // o seu contrutor que cria o objeto
     public Substancia(Tipo tipo, String nome) {
         this.meuTipo = tipo;
         this.nome_susbstancia = nome;
     }
 
-
+    // os get e setter que são utilizados para manipular os atributos do objeto
     public Tipo getMeuTipo() {
         return meuTipo;
     }
@@ -31,6 +31,7 @@ public class Substancia {
         this.nome_susbstancia = nome;
     }
 
+    // trata a resposta , trazendo  os atributos do objeto
     @Override
     public String toString() {
         return "Substancia{" +
