253df87d62207f0d614fb8e41522928ff53ac2ae
Tizuey <89665231+Tizuey@users.noreply.github.com>
2024-05-15 17:19:49 -0300

    Update Farmaceutica.java
    Tizuey -> comentei o código

--- src/main/java/org/farmacia/entities/Farmaceutica.java
+++ src/main/java/org/farmacia/entities/Farmaceutica.java
@@ -1,14 +1,15 @@
 package org.farmacia.entities;
 
+// aqui é a classe de Farmaceutica no qual se criara o objeto
 public class Farmaceutica extends Representante{
 
     public String nome_farmaceutica;
-
+// o seu contrutor que cria o objeto
     public Farmaceutica(String nome_representante, int telefone, String nome_farmaceutica) {
         super(nome_representante, telefone);
         this.nome_farmaceutica = nome_farmaceutica;
     }
-
+    // os get e setter que são utilizados para manipular os atributos do objeto
     public String getNome_farmaceutica() {
         return nome_farmaceutica;
     }
@@ -17,6 +18,7 @@ public class Farmaceutica extends Representante{
         this.nome_farmaceutica = nome_farmaceutica;
     }
 
+    // trata a resposta , trazendo  os atributos do objeto
     @Override
     public String toString() {
         return "Farmaceutica{" +
